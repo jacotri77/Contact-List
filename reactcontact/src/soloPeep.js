2ee12a6ae480e82f15de8a64bef594ffb6cf0dd5
Erik Jacobsen <slowerthanyou@gmail.com>
2017-03-28 21:34:15 -0700

    committing for the night

--- reactcontact/src/soloPeep.js
+++ reactcontact/src/soloPeep.js
@@ -1,35 +1,48 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Users from './userId.json'
+import Users from './userId.json';
 import './index.css';
 
 
 
 
-export default React.createClass({
 
+export default React.createClass({
+  getInitialState() {
+    return{
+      contact: Users.filter(user=>{
+        return Number(user.id) === Number(this.props.match.params.id)
+      })[0]
+
+    }
+  },
+  brokeBack(e){
+    e.preventDefault()
+    this.props.history.goBack()
+  },
 
   render(){
+   var FaUser = require('react-icons/lib/fa/user')
+   var FaEnvelope = require('react-icons/lib/fa/envelope')
+   var FaMobile = require('react-icons/lib/fa/mobile')
+   var FaGlobe = require('react-icons/lib/fa/globe')
+   var FaArrowLeft = require('react-icons/lib/fa/arrow-left')
     return (
      <div id="bigHead">
-        <header>
-          {Users.map(function(user){
-        return <div id="picbox" key={'i'+ user.id}><img src={user.picture.large} alt="#"/></div>
-      })}
-          
-        </header>
-      
+        <div className="photo">
+        <button id="backers" onClick={this.brokeBack}><FaArrowLeft /></button>
+          <img src={this.state.contact.picture.large} alt="#" id="larger"/>
+        </div>
+        <div id="mainBody">
           <ul>
-        {Users.map(function(user){
-        return <div key={'i'+ user.id}><li id="texts" >{user.name.first +" "+user.name.last}</li><li>{user.email}</li><li>{user.cell}</li><li>{user.location.city +" , " + user.location.state}</li></div>
-         
-      })}
-         </ul>
-       
-        
-       </div>
-         
-	
+            <li><FaUser className="iconz"/>{this.state.contact.name.first} {this.state.contact.name.last}</li>
+            <li><FaEnvelope className="iconz"/>{this.state.contact.email}</li>
+            <li><FaMobile className="iconz"/>{this.state.contact.cell}</li>
+            <li><FaGlobe className="iconz"/>{this.state.contact.location.city}, {this.state.contact.location.state}</li>
+          </ul>
+          </div>
+      </div>
+
 )
 }
 })
